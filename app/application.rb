13140a674e8ec73d30d4ee0ecab8dae719117908
Ally Netzel <netzelally@gmail.com>
2018-05-25 19:58:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
-
+  @@cart = []
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
@@ -13,6 +13,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
     else
       resp.write "Path Not Found"
     end
